de69c862ea88807c7dab7dcd9417217c3e6dd856
Dumbhog <charliederham2010@outlook.com>
2025-02-25 17:55:02 -0800

    Update to add Program 2

---
+++ 2. Svelte Practice/SvelteButton.svelte
@@ -0,0 +1,10 @@
+<script>
+    let counter = 0;
+
+    function increment() {
+        counter++
+    }
+</script>
+
+<h1>Click Counter: {counter}</h1>
+<button on:click={increment}>Click me!</button>
